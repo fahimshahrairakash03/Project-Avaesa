90a90b2f0e14b901b19b7852209dc6fda394b17a
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2024-03-17 14:10:42 +0600

    about page setup

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -49,7 +49,7 @@ const Header = () => {
               {menuItems}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">JOUNCE</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{menuItems}</ul>
--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import about from "../../assets/about/About.jpg";
 
 const About = () => {
   return (
     <div>
-      <h1 className="text-xl">About</h1>
+      <div className="hero min-h-screen bg-base-200 px-9">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className="w-3/4">
+            <img src={about} className="max-w-sm rounded-lg shadow-2xl" />
+          </div>
+          <div className="">
+            <h1 className="text-5xl font-bold text-center">About Me</h1>
+            <p className="py-6 text-center my-2">
+              An award winning journalist currently working for the development
+              of the youth towards journalism career.
+            </p>
+            <p className="py-6 text-center my-2">
+              Previously, local demography reportar at Birmingham Mail and BBC
+              and reported at the Gurdian
+            </p>
+            <p className="py-6 text-center my-2">
+              A recipient of the different Trust fellowship and Winner of the
+              Scott Trust bursary 2019
+            </p>
+            <p className="py-6 text-center my-2">
+              Writing specialisms include news, politics, local government,
+              crime, housing and transport.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
